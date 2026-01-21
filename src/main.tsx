123d9e05ba79e087c18ccc784626476773c802f0
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2026-01-21 01:03:51 +0000

    auth removed

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Authenticator } from "@aws-amplify/ui-react";
+//import { Authenticator } from "@aws-amplify/ui-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
